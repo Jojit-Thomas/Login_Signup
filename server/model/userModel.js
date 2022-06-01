71513c580f09890173aac8aa703bb9b3919914d8
Jojit Thomas <thomasjojit@gmail.com>
2022-06-01 23:47:52 +0530

    login are now easy

---
+++ server/model/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 80,
+    }
+})
+
+
+module.exports = mongoose.model('Users', userSchema)
